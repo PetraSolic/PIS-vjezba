bd4a31073996147c8a839d33ced3ad121cc86979
Petra Šolić <solicpetra5@gmail.com>
2026-03-25 19:41:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("PROgram");
+            Console.WriteLine("Promjena");
         }
     }
 }
+
